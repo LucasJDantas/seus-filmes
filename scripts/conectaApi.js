ed65dbf49881d92216ee45063745021731e64fc5
Lucas <lucasjodantas@gmail.com>
2025-07-16 14:41:21 -0300

    Update conectaApi.js

--- scripts/conectaApi.js
+++ scripts/conectaApi.js
@@ -1,4 +1,4 @@
-export const URL_BASE = "http://localhost:3000"
+export const URL_BASE = "https://gist.githubusercontent.com/LucasJDantas/46424a3e5cff1bdf69390226a9b7250f/raw/bd52c40ef4fc016be4067f522b389d746dbb5604/db.json"
 
 export async function listaDeFilmes () {
     const conexao = await fetch(`${URL_BASE}/filmes`);
